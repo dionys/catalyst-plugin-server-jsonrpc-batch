5b3bcc98910cac2c29161ba08b5b89a10bc7ff38
Denis Ibaev <dionys@gmail.com>
2012-05-27 23:01:53 +0400

    Fixed using modules & improved documentation

--- lib/Catalyst/Plugin/Server/JSONRPC/Batch.pm
+++ lib/Catalyst/Plugin/Server/JSONRPC/Batch.pm
@@ -3,90 +3,90 @@ package Catalyst::Plugin::Server::JSONRPC::Batch;
 use strict;
 use warnings;
 
-use Class::MOP ();
+use Class::Load ();
 use HTTP::Body ();
 
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 our $Method = 'system.handle_batch';
 
 
 BEGIN {
-    my $class = 'JSON::RPC::Common::Procedure::Call';
+	my $class = 'JSON::RPC::Common::Procedure::Call';
 
-    Class::MOP::load_class($class) unless Class::MOP::is_class_loaded($class);
+	Class::Load::load_class($class);
 
-    my $meta = $class->meta;
+	my $meta = $class->meta;
 
-    $meta->make_mutable();
-    $meta->add_around_method_modifier(
-        'inflate',
-        sub {
-            my ($meth, $class, @args) = @_;
+	$meta->make_mutable();
+	$meta->add_around_method_modifier(
+		'inflate',
+		sub {
+			my ($meth, $class, @args) = @_;
 
-            if (@args == 1 && ref($args[0]) eq 'ARRAY') {
-                return $class->new_from_data(
-                    jsonrpc => '2.0',
-                    id      => scalar(time()),
-                    method  => $Catalyst::Plugin::Server::JSONRPC::Batch::Method,
-                    params  => $args[0]
-                );
-            }
-            else {
-                return $meth->($class, @args);
-            }
-        }
-    );
-    $meta->make_immutable();
+			if (@args == 1 && ref($args[0]) eq 'ARRAY') {
+				return $class->new_from_data(
+					jsonrpc => '2.0',
+					id      => scalar(time()),
+					method  => $Catalyst::Plugin::Server::JSONRPC::Batch::Method,
+					params  => $args[0]
+				);
+			}
+			else {
+				return $meth->($class, @args);
+			}
+		}
+	);
+	$meta->make_immutable();
 }
 
 
 sub setup_engine {
-    my $app = shift();
+	my $app = shift();
 
-    $app->server->jsonrpc->add_private_method(
-        $Method => sub {
-            my ($c, @args) = @_;
+	$app->server->jsonrpc->add_private_method(
+		$Method => sub {
+			my ($c, @args) = @_;
 
-            my $config = $c->server->jsonrpc->config;
-            my $req    = $c->req;
-            my $res    = $c->res;
-            my $stash  = $c->stash;
-            my $parser = $req->jsonrpc->_jsonrpc_parser;
-            my @results;
+			my $config = $c->server->jsonrpc->config;
+			my $req    = $c->req;
+			my $res    = $c->res;
+			my $stash  = $c->stash;
+			my $parser = $req->jsonrpc->_jsonrpc_parser;
+			my @results;
 
-            # HACK: Store values.
-            my $body = $req->_body;
-            my $path = $config->path;
+			# HACK: Store values.
+			my $body = $req->_body;
+			my $path = $config->path;
 
-            foreach (map { $parser->encode($_) } @{$req->args}) {
-                $config->path('');
-                $stash->{jsonrpc_generated} = 0;
-                $req->_body(HTTP::Body->new($req->content_type, length($_)));
-                $req->_body->add($_);
-                $res->body('');
+			foreach (map { $parser->encode($_) } @{$req->args}) {
+				$config->path('');
+				$stash->{jsonrpc_generated} = 0;
+				$req->_body(HTTP::Body->new($req->content_type, length($_)));
+				$req->_body->add($_);
+				$res->body('');
 
-                $c->prepare_action();
-                $c->dispatch();
-                $stash->{current_view_instance}->process($c)
-                        unless $stash->{jsonrpc_generated};
+				$c->prepare_action();
+				$c->dispatch();
+				$stash->{current_view_instance}->process($c)
+						unless $stash->{jsonrpc_generated};
 
-                push(@results, $res->body);
-            }
+				push(@results, $res->body);
+			}
 
-            # Restore values.
-            $req->_body($body);
-            $config->path($path);
+			# Restore values.
+			$req->_body($body);
+			$config->path($path);
 
-            my $result = '[' . join(',', @results) . ']';
+			my $result = '[' . join(',', @results) . ']';
 
-            $res->content_length(length($result));
-            $res->body($result);
-        }
-    );
+			$res->content_length(length($result));
+			$res->body($result);
+		}
+	);
 
-    $app->next::method(@_);
+	$app->next::method(@_);
 }
 
 
@@ -98,8 +98,8 @@ __END__
 
 =head1 NAME
 
-Catalyst::Plugin::Server::JSONRPC::Batch - Batch calls implementation for
-Catalyst JSONRPC-server plugin.
+Catalyst::Plugin::Server::JSONRPC::Batch - batch requests implementation for
+Catalyst JSON-RPC server plugin.
 
 =head1 SYNOPSIS
 
@@ -111,26 +111,44 @@ Catalyst JSONRPC-server plugin.
 
 =head1 DESCRIPTION
 
-=head1 INTERNAL METHODS
-
-=over 4
-
-=item setup_engine
-
-=back
+Catalyst::Plugin::Server::JSONRPC::Batch implements batch JSON-RPC requests as
+its described in specification version 2.0.
 
 =head1 SEE ALSO
 
-L<JSON-RPC 2.0 Specification|http://www.jsonrpc.org/specification>
+L<Catalyst>, L<Catalyst::Plugin::Server::JSONRPC>.
 
-=head1 AUTHORS
+L<JSON-RPC 2.0 Specification|http://www.jsonrpc.org/specification>.
 
-Denis Ibaev (dionys), C<dionys@gmail.com>
+=head1 SUPPORT
 
-=head1 COPYRIGHT & LICENSE
+=over 4
 
-Copyright (c) 2011 the aforementioned authors. All rights reserved. This
-program is free software; you can redistribute it and/or modify it under the
-same terms as Perl itself.
+=item Repository
+
+L<http://github.com/dionys/catalyst-plugin-server-jsonrpc-batch>
+
+=item Bug tracker
+
+L<http://github.com/dionys/catalyst-plugin-server-jsonrpc-batch/issues>
+
+=back
+
+=head1 AUTHOR
+
+Denis Ibaev, C<dionys@cpan.org>.
+
+=head1 THANKS TO
+
+Ivan Fomichev (IFOMICHEV), Sergey Romanov.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012, Denis Ibaev.
+
+This program is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+See L<http://dev.perl.org/licenses/> for more information.
 
 =cut
